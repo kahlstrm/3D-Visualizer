0b48e0a76c1bbceaa334de19a2eace12f2d06a34
Kalle Ahlström <hashtagkalski@gmail.com>
2022-03-30 17:57:13 +0300

    initial MainFrame & listeners

---
+++ src/main/scala/visualizer/Math.scala
@@ -0,0 +1,5 @@
+package visualizer
+
+class Math {
+  
+}
--- src/main/scala/visualizer/VisualizerApp.scala
+++ src/main/scala/visualizer/VisualizerApp.scala
@@ -1,6 +1,29 @@
 package visualizer
 import scala.swing._
-object VisualizerApp extends App{
-  for(n<-1 to 50)
-
+import scala.swing.event._
+object VisualizerApp extends SimpleSwingApplication{
+  val width= 800
+  val height= 600
+  def top = new MainFrame{
+    title="3d-visualizer"
+    minimumSize = new Dimension(width,height)
+    resizable=false
+    
+    val area = new Panel {
+      focusable=true
+      override def paintComponent(g: Graphics2D) = {
+        g.setColor(new Color(0,0,0))
+        g.fillRect(0,0, width,height)
+        
+      }
+    }
+    contents = area
+    listenTo(area.mouse.clicks)
+    listenTo(area.keys)
+    reactions +={
+      case KeyPressed(src,Key.Escape,_,_)=>println("bye");scala.sys.exit()
+      case KeyPressed(src,key,_,_)=>println(key)
+      case MousePressed(src,point,_,_,_)=>println("hello team")
+    }
+  }
 }
