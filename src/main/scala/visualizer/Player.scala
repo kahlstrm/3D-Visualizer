ffd9a905d3b362d23877ca1ebebb89bc7508cddd
Kalle Ahlström <hashtagkalski@gmail.com>
2022-03-31 02:42:53 +0300

    add stable constant movement

--- src/main/scala/visualizer/Player.scala
+++ src/main/scala/visualizer/Player.scala
@@ -1,8 +1,25 @@
 package visualizer
 
 object Player {
-val pos = new Pos(0,0,0)
-  
+  val pos = new Pos(0, 0, 0)
 
-override def toString(): String = s"Player is currently at ${pos}"
+  def move() {
+    if (moveForward) {
+      pos.addZ(1)
+    }
+    if (moveBackward) {
+      pos.addZ(-1)
+    }
+    if (moveLeft) {
+      pos.addX(-1)
+    }
+    if (moveRight) {
+      pos.addX(1)
+    }
+  }
+  var moveForward = false
+  var moveBackward = false
+  var moveLeft = false
+  var moveRight = false
+  override def toString(): String = s"Player is currently at ${pos}"
 }
--- src/main/scala/visualizer/VisualizerApp.scala
+++ src/main/scala/visualizer/VisualizerApp.scala
@@ -5,24 +5,25 @@ import scala.collection.mutable.Buffer
 import java.awt.Color
 import java.awt.event.ActionListener
 
-object VisualizerApp extends SimpleSwingApplication{
-  val width= 800
-  val height= 600
-  val windowHeight=630
-  val aspectRatio = 800.toDouble/600
+object VisualizerApp extends SimpleSwingApplication {
+  val width = 800
+  val height = 600
+  val windowHeight = 630
+  val aspectRatio = 800.toDouble / 600
 
-  var triangles=Seq[Triangle]()
-  val posBuffer= Buffer[Pos]()
-  def top = new MainFrame{
-    title="3d-visualizer"
-    minimumSize = new Dimension(width,windowHeight)
-    resizable=false
-    
+  var triangles = Seq[Triangle]()
+  val posBuffer = Buffer[Pos]()
+  def top = new MainFrame {
+    title = "3d-visualizer"
+    minimumSize = new Dimension(width, windowHeight)
+    resizable = false
+
     val area = new Panel {
-      focusable=true
+      focusable = true
       override def paintComponent(g: Graphics2D) = {
+        println(Player)
         g.setColor(Color.BLACK)
-        g.fillRect(0,0, width,height)
+        g.fillRect(0, 0, width, height)
         g.setColor(Color.WHITE)
         triangles.foreach(_.draw(g))
       }
@@ -30,37 +31,43 @@ object VisualizerApp extends SimpleSwingApplication{
     contents = area
     listenTo(area.mouse.clicks)
     listenTo(area.keys)
-    reactions +={
-      case KeyPressed(_,key,_,_)=>{
+    reactions += {
+      case KeyPressed(_, key, _, _) => {
         key match {
-          case Key.Escape=>println("bye");scala.sys.exit()
-          case Key.W=>Player.pos.addZ(5)
-          case Key.S=>Player.pos.addZ(-5)
-          case Key.A=>Player.pos.addX(-5)
-          case Key.D=>Player.pos.addX(5)
-          case a=>println(a)
+          case Key.Escape => println("bye"); scala.sys.exit()
+          case Key.W      => Player.moveForward = true
+          case Key.S      => Player.moveBackward = true
+          case Key.A      => Player.moveLeft = true
+          case Key.D      => Player.moveRight = true
+          case a          => println(a)
         }
       }
-      case KeyReleased(_,key,_,_)=>{
-        
+      case KeyReleased(_, key, _, _) => {
+        key match {
+          case Key.W => Player.moveForward = false
+          case Key.S => Player.moveBackward = false
+          case Key.A => Player.moveLeft = false
+          case Key.D => Player.moveRight = false
+        }
       }
-      case MousePressed(_,point,_,_,_)=>{
-      println(point)
-      posBuffer+=new Pos(point.x,point.y,0)
-      if(posBuffer.size>=3){
-        triangles=triangles:+new Triangle(posBuffer(0),posBuffer(1),posBuffer(2))
-        posBuffer.clear()
-      }
+      case MousePressed(_, point, _, _, _) => {
+        println(point)
+        posBuffer += new Pos(point.x, point.y, 0)
+        if (posBuffer.size >= 3) {
+          triangles =
+            triangles :+ new Triangle(posBuffer(0), posBuffer(1), posBuffer(2))
+          posBuffer.clear()
+        }
       }
     }
 
-
-    val listener = new ActionListener(){
-      def actionPerformed(e : java.awt.event.ActionEvent) = {
-        area.repaint() 
-      }  
+    val listener = new ActionListener() {
+      def actionPerformed(e: java.awt.event.ActionEvent) = {
+        Player.move()
+        area.repaint()
+      }
     }
-    val timer = new javax.swing.Timer(6,listener)
+    val timer = new javax.swing.Timer(6, listener)
     timer.start()
   }
 }
