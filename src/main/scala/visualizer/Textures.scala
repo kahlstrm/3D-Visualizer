5e47ccb04b2de0afdd33a2712299caf38aeb06d1
Kalle Ahlström <hashtagkalski@gmail.com>
2022-05-19 14:05:07 +0300

    make textures repeatable

--- src/main/scala/visualizer/Textures.scala
+++ src/main/scala/visualizer/Textures.scala
@@ -8,17 +8,17 @@ case class Texture(image: BufferedImage) {
 
   // returns colors
   def getColorPixel(x: Int, y: Int): Int = {
-    if (x >= width || x < 0 || y >= height || x < 0) {
-      println(f"x:$x y:$y out of range")
-      return 0
-    }
-    return texturePixels.getElem(x + y * width)
+    // if (x >= width || x < 0 || y >= height || x < 0) {
+    //   println(f"x:$x y:$y out of range")
+    //   return 0
+    // }
+    return texturePixels.getElem(x%width + y%height * width)
   }
   def getColor(x: Float, y: Float): Int = {
-    if (x > 1 || x < 0 || y > 1 || y < 0) {
-      // println(f"x:$x y:$y out of range")
-      return 0
-    }
+    // if (x > 1 || x < 0 || y > 1 || y < 0) {
+    //   // println(f"x:$x y:$y out of range")
+    //   return 0
+    // }
     val xPixel = Math.max(0, Math.ceil(x * width).toInt - 1)
     val yPixel = Math.max(0, Math.ceil(y * height).toInt - 1)
     return getColorPixel(xPixel, yPixel)
