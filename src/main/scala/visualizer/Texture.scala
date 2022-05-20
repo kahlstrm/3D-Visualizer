87f4e6e4433af51e55635050d74ec95d062087b9
Kalle Ahlström <hashtagkalski@gmail.com>
2022-05-20 06:26:30 +0300

    small refactor

--- src/main/scala/visualizer/Texture.scala
+++ src/main/scala/visualizer/Texture.scala
@@ -5,26 +5,18 @@ import java.awt.image.BufferedImage
 // simple texture class that provides color information
 class Texture(image: BufferedImage) {
   private lazy val texturePixels = image.getData().getDataBuffer()
+  private val width = image.getWidth()
+  private val height = image.getHeight()
 
   // returns colors
   def getColorPixel(x: Int, y: Int): Int = {
-    // if (x >= width || x < 0 || y >= height || x < 0) {
-    //   println(f"x:$x y:$y out of range")
-    //   return 0
-    // }
     return texturePixels.getElem(x%width + y%height * width)
   }
   def getColor(x: Float, y: Float): Int = {
-    // if (x > 1 || x < 0 || y > 1 || y < 0) {
-    //   // println(f"x:$x y:$y out of range")
-    //   return 0
-    // }
     val xPixel = Math.max(0, Math.ceil(x * width).toInt - 1)
     val yPixel = Math.max(0, Math.ceil(y * height).toInt - 1)
     return getColorPixel(xPixel, yPixel)
   }
-  private val width = image.getWidth()
-  private val height = image.getHeight()
 }
 object Texture{
   def apply(imageSource:String):Texture={
